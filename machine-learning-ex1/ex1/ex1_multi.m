47e348d77907e894eeefcb091298f05e0600129b
Javier Fonseca V <fonsecajavier@gmail.com>
2018-08-18 22:46:39 -0500

    Solves ex1

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -87,11 +87,27 @@ num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
+theta2 = zeros(3, 1);
+theta3 = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+[theta2, J_history2] = gradientDescentMulti(X, y, theta2, 1, num_iters);
+[theta3, J_history3] = gradientDescentMulti(X, y, theta3, 0.003, num_iters);
 
 % Plot the convergence graph
 figure;
+hold on
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+disp('theta:')
+fprintf(' %f \n', theta);
+
+plot(1:numel(J_history2), J_history2, 'r', 'LineWidth', 2);
+disp('theta2:')
+fprintf(' %f \n', theta2);
+
+plot(1:numel(J_history3), J_history3, 'k', 'LineWidth', 2);
+disp('theta3:')
+fprintf(' %f \n', theta3);
+
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,7 +120,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+x = [1 (([1650 3] - mu) ./ sigma)];
+price = x*theta;
 
 
 % ============================================================
@@ -149,8 +166,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+x = [1 1650 3];
+price = x * theta;
 
 % ============================================================
 
